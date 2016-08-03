4b8a221acedc86d8b1f400c34cbad2fd494a4554
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-08-03 17:08:19 +0700

    simple is best for syn pop states

--- syn_pop.R
+++ syn_pop.R
@@ -8,16 +8,13 @@
 syn_pop <- function(states){ #states is the vector variable, each element represent the number of individuals belonging to that state (indexed)
   #this is assuming that an individual can have only one state at a single timestep
   
-  no.states <- length(states) #number of states, decide the columns
   total.pop <- sum(states) #total population size
   
-  probs <- states/total.pop #probabilities of being in the specific states
-  
-  result <- sample(0:1, total.pop, replace=T, prob=c(1-probs[1], probs[1])) #to store the resulting matrix, columns will be binded here in the following for loop
+  result <- c(rep(1,states[1]), rep(0,total.pop - states[1])) #to store the resulting matrix, columns will be binded here in the following for loop
   
   
-  for(i in 2:no.states){
-    tmp <- sample(0:1, total.pop, replace=T, prob=c(1-probs[i], probs[i])) #tmp to store the result before cbinding
+  for(i in 2:length(states)){
+    tmp <- c(rep(0,sum(states[1:(i-1)])), rep(1, states[i]) , rep(0,total.pop - sum(states[1:i]) )) #tmp to store the result before cbinding
     
     result <- cbind(result, tmp, deparse.level = 0)
   }
